6bd70cd0b2de00de426f6c181e42199f17a70784
Dev-SardarAbdul <dev.sardarabdul@gmail.com>
2023-12-28 22:25:28 +0500

    changes

--- frontend/src/components/footer.jsx
+++ frontend/src/components/footer.jsx
@@ -4,7 +4,7 @@ import { FaCcMastercard } from "react-icons/fa6";
 
 function Footer() {
   return (
-    <div className="py-6 ">
+    <div className="py-6 bg-[##F7F5F8]">
       <div className="flex items-center justify-between main-container">
         <p className="text-xl secondary-font font-[500]">
           © {[new Date().getFullYear()]} Calibr
--- frontend/src/pages/home/hero.jsx
+++ frontend/src/pages/home/hero.jsx
@@ -53,11 +53,11 @@ function Hero() {
           <SwiperSlide>
             <div className={`relative min-h-screen`}>
               <img
-                className="absolute inset-0 w-full h-full object-cover"
+                className="absolute inset-0 object-cover w-full h-full"
                 src={item.image}
               />
-              <div className="absolute h-full w-full flex flex-col justify-center items-center ">
-                <div className="main-container w-full h-full flex flex-col justify-center items-center gap-4">
+              <div className="absolute flex flex-col items-center justify-center w-full h-full ">
+                <div className="flex flex-col items-center justify-center w-full h-full gap-4 main-container">
                   <h4 className="text-white text-[18px] md:text-[30px] font-[600] uppercase text-center">
                     {item.topText}
                   </h4>
@@ -67,19 +67,19 @@ function Hero() {
                   <h1 className="text-center text-white text-[50px] sm:text-[80px] md:text-[120px] font-[600] secondary-font leading-[40px] sm:leading-[70px] md:leading-[100px]">
                     {item.price}
                   </h1>
-                  <button className="bg-secondary text-white mt-4 px-8 md:px-12 py-4 text-[16px] md:text-[24px] font-[600] ">
+                  <button className="bg-secondary text-white mt-4 px-8 md:px-12 py-4 text-[16px] md:text-[24px] font-[600] hover:bg-white hover:text-secondary duration-500">
                     Shop Now
                   </button>
 
-                  <div className="mt-4 flex justify-center items-center gap-4 md:hidden">
+                  <div className="flex items-center justify-center gap-4 mt-4 md:hidden">
                     <div
-                      className="size-8 bg-secondary justify-center items-center rounded-sm cursor-pointer flex"
+                      className="flex items-center justify-center rounded-sm cursor-pointer size-8 bg-secondary"
                       onClick={() => swiperRef.current?.slidePrev()}
                     >
                       <IoIosArrowBack className="text-white text-[24px]" />
                     </div>
                     <div
-                      className="size-8 bg-secondary justify-center items-center rounded-sm cursor-pointer flex"
+                      className="flex items-center justify-center rounded-sm cursor-pointer size-8 bg-secondary"
                       onClick={() => swiperRef.current?.slideNext()}
                     >
                       <IoIosArrowForward className="text-white text-[24px]" />
