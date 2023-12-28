af0b032408a8439c4b1dc9a4ffa6bd68e8ce32fa
Dev-SardarAbdul <dev.sardarabdul@gmail.com>
2023-12-28 21:52:16 +0500

    homepage added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Home } from "./pages";
+import { Footer } from "./components";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/shop" element={<h2>Shop</h2>} />
         </Routes>
+        <Footer />
       </BrowserRouter>
     </div>
   );
--- frontend/src/components/footer.jsx
+++ frontend/src/components/footer.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import { FaCcAmex, FaCcDiscover, FaCcVisa } from "react-icons/fa";
+import { FaCcMastercard } from "react-icons/fa6";
 
 function Footer() {
-  return <div>Footer</div>;
+  return (
+    <div className="py-6 ">
+      <div className="flex items-center justify-between main-container">
+        <p className="text-xl secondary-font font-[500]">
+          © {[new Date().getFullYear()]} Calibr
+        </p>
+        <div className="flex items-center justify-end gap-4">
+          <FaCcVisa className="text-[24px] hover:text-secondary" />
+          <FaCcMastercard className="text-[24px] hover:text-secondary" />
+          <FaCcAmex className="text-[24px] hover:text-secondary" />
+          <FaCcDiscover className="text-[24px] hover:text-secondary" />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Footer;
