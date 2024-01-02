a7da43f9655f034ac38bcc22eae7b3ba7a9494d8
Dev-SardarAbdul <dev.sardarabdul@gmail.com>
2024-01-03 03:37:24 +0500

    firebase added to project

---
+++ frontend/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCyHAKAg-GgjYpDHKZ9j1Kx0eAR_zMSazo",
+  authDomain: "calibr-37b27.firebaseapp.com",
+  projectId: "calibr-37b27",
+  storageBucket: "calibr-37b27.appspot.com",
+  messagingSenderId: "534680157172",
+  appId: "1:534680157172:web:3302c3d616c433ab4c57a1",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
