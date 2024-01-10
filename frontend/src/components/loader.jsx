97da7e5795b2f7f5ba77d7f170545cf90224abaa
Dev-SardarAbdul <dev.sardarabdul@gmail.com>
2024-01-10 19:48:21 +0500

    loader styles fixed

--- frontend/src/components/loader.jsx
+++ frontend/src/components/loader.jsx
@@ -3,7 +3,7 @@ import PistolAnimation from "./pistolAnimation";
 
 function Loader() {
   return (
-    <div className="fixed flex top-0 left-0 flex-col items-center justify-center w-full h-full bg-black bg-opacity-70 z-[100]">
+    <div className="fixed flex top-0 left-0 flex-col items-center justify-center w-full h-full  backdrop-blur-[5px] bg-black bg-opacity-15 z-[100]">
       <PistolAnimation />
     </div>
   );
